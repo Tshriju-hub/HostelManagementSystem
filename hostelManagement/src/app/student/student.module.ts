fe024276c9d46fd2d8ea4e2f2a1a5defe6747100
Tshriju-hub <shreejuthapa7@gmail.com>
2024-05-14 19:24:43 +0545

    completed ui of student dashboard

--- hostelManagement/src/app/student/student.module.ts
+++ hostelManagement/src/app/student/student.module.ts
@@ -6,13 +6,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from './user.service';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { StudentAddComponent } from './student-add/student-add.component';
-import { ViewAllStudentComponent } from './view-all-student/view-all-student.component';
-import { UpdateStudentComponent } from './update-student/update-student.component';
 import { FoodMenuComponent } from './food-menu/food-menu.component';
-import { InquiryComponent } from './inquiry/inquiry.component';
-import { PricesComponent } from './prices/prices.component';
-import { UsersComponent } from './users/users.component';
 import { RoomStructureComponent } from './room-structure/room-structure.component';
 
 
@@ -20,13 +14,7 @@ import { RoomStructureComponent } from './room-structure/room-structure.componen
   declarations: [
     DashboardComponent,
     SidebarComponent,
-    StudentAddComponent,
-    ViewAllStudentComponent,
-    UpdateStudentComponent,
     FoodMenuComponent,
-    InquiryComponent,
-    PricesComponent,
-    UsersComponent,
     RoomStructureComponent
   ],
   imports: [
