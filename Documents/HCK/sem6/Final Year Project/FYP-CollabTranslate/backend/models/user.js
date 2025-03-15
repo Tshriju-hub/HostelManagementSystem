b9f2a3b968d85c55d47d9970a66c34cae8aac85a
upasanapaudel9 <upasanapaudel9@gmail.com>
2025-03-15 11:57:10 +0545

    Set up database connection and define User, Post models

---
+++ Documents/HCK/sem6/Final Year Project/FYP-CollabTranslate/backend/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("moongoose");
+
+const user = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    address:{
+        type: String,
+        required: true,
+    },
+    avatar:{
+        type: String,
+        default: "https://cdn-icons-png.flaticon.com/128/3177/3177440.png",
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin", "translator", "editor"],
+    },
+    favourites: [
+        {
+         type: mongoose.Types.ObjectId,
+         ref:"webnovels",
+        },
+    ],
+},
+ { timestamps: true }
+);
+module.exports = mongoose.model("user", user)
